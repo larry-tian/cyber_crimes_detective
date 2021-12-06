0caef13aecdda068dd3dd830160f24ad4e61e6ab
Larry-Tian <ganlin@uw.edu>
2021-12-05 16:12:58 -0800

    Finished the layout

--- cyber-crimes_detective/src/components/nav/Nav.js
+++ cyber-crimes_detective/src/components/nav/Nav.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
+import logoImg from '../../img/logo.png'
+import logoHoverImg from '../../img/logo_hover.png'
+
+import './Nav.css'
 const Nav = () => {
 
   return (
     <div>
-        <h1>Cybercrime Detective</h1>
+        <nav className="navbar">
+            <ul className="nav-layout">
+              <img className="logo" src={logoImg} onMouseOver={e => (e.currentTarget.src= logoHoverImg)} onMouseOut={e => (e.currentTarget.src = logoImg)}/>
+              <h1 className="logo-name">Cybercrime Detective</h1>
+            </ul>
+        </nav>
     </div>
   )
 };
